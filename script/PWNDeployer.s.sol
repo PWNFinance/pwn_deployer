03ccb3e005cc40d47c50a18c3a98c5c20cb031ca
ashhanai <ashhab.naim@gmail.com>
2023-06-30 20:17:52 +0100

    Update script with safe deployment

--- script/PWNDeployer.s.sol
+++ script/PWNDeployer.s.sol
@@ -6,6 +6,29 @@ import "forge-std/Script.sol";
 import "@pwn_deployer/PWNDeployer.sol";
 
 
+interface GnosisSafeProxyFactoryLike {
+    function createProxyWithNonce(
+        address _singleton,
+        bytes memory initializer,
+        uint256 saltNonce
+    ) external returns (address proxy);
+}
+
+interface GnosisSafeProxyLike {
+    function setup(
+        address[] calldata _owners,
+        uint256 _threshold,
+        address to,
+        bytes calldata data,
+        address fallbackHandler,
+        address paymentToken,
+        uint256 payment,
+        address payable paymentReceiver
+    ) external;
+}
+
+contract Deploy is Script {
+
 /*
 
 forge script script/PWNDeployer.s.sol:Deploy \
@@ -15,14 +38,42 @@ forge script script/PWNDeployer.s.sol:Deploy \
 --broadcast
 
 */
-contract Deploy is Script {
-
     function run() external {
         vm.startBroadcast();
 
         // Deploy via `0x0cfC62...C8D6de` EOA to have the same address on all networks
         PWNDeployer deployer = new PWNDeployer();
         console2.log("Deployer address:", address(deployer));
+
+        vm.stopBroadcast();
+    }
+
+
+/*
+
+forge script script/PWNDeployer.s.sol:Deploy \
+--sig "deploySafe(address,address,address,uint256)" $SAFE_PROXY_FACTORY $SAFE_SINGLETON $FALLBACK_HANDLER $SALT \
+--rpc-url $RPC_URL \
+--private-key $PRIVATE_KEY \
+--verify --etherscan-api-key $ETHERSCAN_API_KEY \
+--broadcast
+
+*/
+    function deploySafe(address safeProxFactory, address safeSingleton, address fallbackHandler, uint256 salt) external {
+        vm.startBroadcast();
+
+        address[] memory owners = new address[](1);
+        owners[0] = 0x0cfC62C2E82dA2f580Fd54a2f526F65B6cC8D6de;
+
+        address safe = GnosisSafeProxyFactoryLike(safeProxFactory).createProxyWithNonce({
+            _singleton: safeSingleton,
+            initializer: abi.encodeWithSelector(
+                GnosisSafeProxyLike.setup.selector,
+                owners, 1, address(0), "", fallbackHandler, address(0), 0, payable(address(0))
+            ),
+            saltNonce: salt
+        });
+        console2.log("Safe address:", safe);
 
         vm.stopBroadcast();
     }
